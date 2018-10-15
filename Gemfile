f2a8c4f512391cf2bf057b3d20a0965374a0c8cf
Clare Jolly <clarejollyuk@gmail.com>
2018-10-15 16:44:29 +0100

    cat form page

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'rubocop', '0.56.0'
 
 gem 'sinatra', '~> 1.4', '>= 1.4.7'
 gem 'shotgun', '~> 0.9.2'
+gem 'capybara', '~> 3.9'
+gem 'selenium-webdriver', '~> 3.14', '>= 3.14.1'
+gem 'chromedriver-helper'
 
 group :test do
   gem 'rspec'
--- app.rb
+++ app.rb
@@ -15,8 +15,14 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+# in app.rb
+# first, let's write a form-displaying route
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
 end
